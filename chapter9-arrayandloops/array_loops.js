5cf3812b96b6f0f1aad6bbbc698475ad0d7d41ec
Santosh Shah <santoshofficial679@gmail.com>
2023-11-28 16:17:37 +0545

    Array completed

---
+++ chapter9-arrayandloops/array_loops.js
@@ -0,0 +1,47 @@
+// const cars = [
+//   "Saab",
+//   "Volvo",
+//   "Maruti"
+// ];
+
+// const cars = [];
+// cars[0] = "Maruti";
+// cars[2] = "Tayota";
+// cars[5] = "BMW";
+
+const cars = new Array("Car1", "Car2", "Car3");
+
+// we can also change value
+// cars[2] = "car5";
+
+// document.querySelector("#arrayPrint").innerText = cars[2];
+
+// document.querySelector("#arrayPrint").innerText = cars;
+
+// converting an array to a string
+// document.querySelector("#arrayPrint").innerText = cars.toString();
+
+// let str = cars.toString();
+// console.log(typeof cars);
+
+
+
+// we can store objects, function etc in arrays;
+// let myDate = new Date();
+// function funFunction() {
+//   return "This function of funn";
+// }
+
+// const justFun = [];
+// justFun[0] = myDate.getFullYear();
+// justFun[1] = funFunction();
+// justFun[2] = cars;
+
+// document.querySelector("#arrayPrint").innerText = justFun[2];
+
+// console.log(cars.length);
+// console.log(cars.splice(0, 2, "car0", "car10"));
+// console.log(cars);
+// console.log(cars.reverse());
+console.log(cars);
+console.log(cars.slice(1));
