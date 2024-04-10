f9fae7799d1a1f7869c65bd9b628b3cbedbe6dcb
Santosh Shah <santoshofficial679@gmail.com>
2024-04-11 01:10:47 +0545

    Conversion of datatypes

---
+++ day5-datatypes/script.js
@@ -0,0 +1,37 @@
+// let nameValue = "Santosh Shah"
+// let number = 4521
+// let youAreRight = true
+// let youAreWrong = false
+// let nullValue = null
+// let undefinedValue = undefined
+
+
+// console.log(typeof nameValue)
+// console.log(typeof number)
+// console.log(typeof youAreRight)
+// console.log(typeof youAreRight)
+// console.log(typeof nullValue)
+// console.log(typeof undefinedValue)
+
+// alert("Alter brother it's me");
+
+
+
+//point to be noted
+// let score = "1524age"    // NaN
+// let score = null         // 0
+// let score = undefined       // NaN
+// let score = true            // 1
+// let score = false           // 0
+// let numberValue = Number(score);
+// console.log(typeof score);
+// console.log(typeof numberValue);
+// console.log(numberValue);
+
+
+let isLoggedIn = 0
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+console.log(isLoggedIn);
+console.log(typeof isLoggedIn );
+console.log(boolenIsLoggedIn);
+console.log(typeof boolenIsLoggedIn);
