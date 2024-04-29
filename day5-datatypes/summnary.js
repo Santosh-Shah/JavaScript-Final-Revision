a0f7370667a8263f835ae699dcb84017a6bde0eb
Santosh Shah <santoshofficial679@gmail.com>
2024-04-29 23:57:42 +0545

    data type summanry Done

---
+++ day5-datatypes/summnary.js
@@ -0,0 +1,14 @@
+// Types of data type
+
+// 1) Primitive
+// 7 types
+        // String, Number, Boolean, null, undefined, Symbol
+
+
+// 2) Non Primitive(Reference)
+        // Array, Objects, Functions
+
+
+const id = Symbol('123');
+console.log(typeof id)
+console.log(id)
