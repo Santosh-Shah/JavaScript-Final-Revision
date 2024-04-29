7c1172abe3c6975cf8e7888da9ac0beb55ec726f
Santosh Shah <santoshofficial679@gmail.com>
2024-04-30 00:21:21 +0545

    Stack and Heap concept cleared

---
+++ day5-datatypes/stack_heap.js
@@ -0,0 +1,28 @@
+// Stack(primitive data type)
+// Heap(referenced/non primitive data types)
+
+
+
+// Note: you will get only copy of primitive data types into stack
+// eg:
+let userName1 = "Hariom Shah";
+let userName2 = userName1;
+
+userName2 = "Santosh Shah";
+
+console.log(userName1);
+console.log(userName2);
+
+
+
+// let  userOne = {
+//     name : "Hariom Shah",
+//     phone : "9811243552"
+// }
+
+// let userTwo = userOne
+
+// userTwo.phone = "9849697391"
+
+// console.log(userOne)
+// console.log(userTwo)
