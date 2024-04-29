9945be9eace8b83291b49c0a08cb1e8273e734de
Santosh Shah <santoshofficial679@gmail.com>
2024-04-29 21:24:50 +0545

    Conversion of datatypes

---
+++ day5-datatypes/datatypes_conversion.js
@@ -0,0 +1,33 @@
+// let score = "abc33";
+// let score = null
+// let score = true
+// let score = false;
+// console.log(score)
+// console.log(typeof(score))
+
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = 0
+// let isLoggedIn = ""
+// let isLoggedIn = "Hariom"
+// let boolenIsLoggedIn = Boolean(isLoggedIn)
+// console.log(boolenIsLoggedIn)
+// console.log(typeof boolenIsLoggedIn)
+
+// 1 => true,     0 => falese
+// " " => false,  "Hariom" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
