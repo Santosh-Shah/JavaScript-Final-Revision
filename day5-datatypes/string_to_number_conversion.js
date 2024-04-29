1b2ea50dbd665846323ab1b85cd777a3bcc3d28f
Santosh Shah <santoshofficial679@gmail.com>
2024-04-29 23:11:20 +0545

    comparison completed

---
+++ day5-datatypes/string_to_number_conversion.js
@@ -0,0 +1,18 @@
+let str1 = "Vedas"
+let str2 = " College"
+let str3 = str1 + str2
+
+// console.log(str3)
+// console.log("1")
+// console.log(typeof "1")
+
+console.log(1 + "2")
+console.log(typeof 1 + "2")
+
+console.log("1" + 3)
+console.log(typeof "1" + 3)
+
+console.log("1" + 2 + 2)
+console.log(typeof "1" + 2 + 2)
+
+console.log(1 + 2 + "2")
