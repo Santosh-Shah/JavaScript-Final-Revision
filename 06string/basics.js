f83b33c0ceb0c668e6e617a79536d909a7f5f5ec
Santosh Shah <santoshofficial679@gmail.com>
2024-05-03 01:00:57 +0545

    String completed

---
+++ 06string/basics.js
@@ -0,0 +1,38 @@
+const name = "santosh"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const myName = new String("Santosh")
+// console.log(myName);
+// console.log(myName[1]);
+// console.log(myName[0]);
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
+// console.log(myName);
+
+// console.log("---------------------")
+
+// console.log(myName.charAt(2));
+// console.log(myName.indexOf('t'));
+
+// const newString = myName.substring(1, 4);
+// console.log(newString);
+
+// const anottherString = myName.slice(-2, 6);
+// console.log(anottherString);
+
+
+const greeting = "   Vedas College    ";
+console.log(greeting);
+console.log(greeting.trim());
+
+const url = "https://santosh.com/santosh%20shah"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('com'));
+
+const words = greeting.split();
+console.log(words);
