8a764f388df3d6c39ce8e9273c7598f9fe478b41
Santosh Shah <santoshofficial679@gmail.com>
2024-05-31 02:56:07 +0545

    array hight order methods completed

---
+++ day12_highOrderArrayMethods/overview.js
@@ -0,0 +1,21 @@
+const numbers = [4, 5, 6, 2, 40];
+// numbers.forEach(value => console.log(value));
+
+//using map
+// const squared = numbers.map(value => value * value);
+// console.log(squared);
+
+// using filter
+// const evenNumber = numbers.filter(value => value % 2 == 0);
+// console.log(evenNumber);
+
+// using reduce
+// const sum = numbers.reduce((total, value) => total + value, 2);
+// console.log(sum);
+
+
+// using sort
+numbers.sort()
+console.log(numbers);
+
+
