fe8d8cba02bd63e4a38ffad0e3f371520367a803
Santosh Shah <santoshofficial679@gmail.com>
2024-05-31 02:45:40 +0545

    control statement completed

---
+++ control_statement/controlStatements.js
@@ -0,0 +1,77 @@
+// only if statement
+// let age = 12;
+// if (age >= 25) {
+//     console.log("it more than 25 or equal to 25!");
+// }
+
+
+// if and else statement
+// let age = 12;
+// if (age >= 25) {
+//     console.log("it more than 25 or equal to 25!");
+// } else {
+//     console.log("it less than 25");
+// }
+
+
+// let age = 20;
+// if (age < 13) {
+//     console.log("You are a chile")
+// } else if (age < 20) {
+//     console.log("You are a teenage");
+// } else {
+//     console.log("You are an adult");
+// }
+
+
+// let dayName;
+// let day = 3;
+// switch (day) {
+//     case 2:
+//         dayName = "Monday";
+//         break;
+//     case 3:
+//         dayName = "Tuesday";
+//         break;
+//     case 4:
+//         dayName = "Wednesday";
+//         break;
+//     default:
+//         dayName = "Non of the day";
+// }
+
+// console.log(dayName);
+
+
+
+// For Loop
+// for (let i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+
+
+// while loop
+// let i = 0;
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+
+// do while loop
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 6);
+
+
+
+// Break statement
+for (let i = 0; i < 10; i++) {
+    if (i == 5) {
+        // break;
+        continue;
+    }
+    console.log(i);
+}
