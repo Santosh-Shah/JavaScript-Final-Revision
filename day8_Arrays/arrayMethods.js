781bfd6f5de24e6a8b4e241ef33a311424cca90b
Santosh Shah <santoshofficial679@gmail.com>
2024-05-30 21:31:21 +0545

    Array methods completed

--- day8_Arrays/arrayMethods.js
+++ day8_Arrays/arrayMethods.js
@@ -57,3 +57,66 @@ console.log("\n--------The slice() method returns a new array containing a porti
 const oldFruits = ["Banana", "Orange", "Apple", "Mango"];
 const citrus = oldFruits.slice(1, 3);
 console.log(citrus); // Outputs: ["Orange", "Apple"]
+
+
+
+
+
+console.log("\n---------------Searching Methods--------------")
+const array1 = ["Apple", "Banana", "Orange", "Apple"];
+console.log(array1.indexOf("Apple"));
+
+console.log(array1.lastIndexOf("Apple"));
+console.log(array1.includes("Applee"));
+// console.log(array1.sort());
+console.log(array1.reverse());
+array1.forEach(num => console.log(num));
+
+const numb = [34, 67, 1, 10, 20, 11, 10];
+const found = numb.find(elem => elem == 1);
+console.log(found);
+
+console.log(numb.findIndex(elem => elem == 10));
+
+
+
+
+
+
+
+console.log("\n------------------JavaScript Sorting Arrays-----------------------");
+const cars = ["Car1", "Car4", "Car0", "Car3", "Car2"];
+// console.log(cars.sort());
+
+const numbers = [0, 45, 20, 11, 23];
+// console.log(numbers.sort());
+
+// console.log(cars.reverse());
+console.log(cars);
+
+const carss = [
+    { make: "Volvo", year: 2016 },
+    { make: "BMW", year: 2019 },
+    { make: "Saab", year: 2008 }
+  ];
+
+console.log(carss);
+console.log(carss.sort((a, b) => a.year - b.year));
+
+
+
+
+
+console.log("\n-------------------Arrays Iteration Methods-----------------------");
+const veg = ["veg1", "veg2", "veg3", "veg4", "veg5"];
+veg.forEach((value, index) => {console.log(index, value)});
+
+
+const orgArray = [4, 6, 3, 4];
+const doubledArray = orgArray.map(num => num * 2);
+console.log(doubledArray);
+
+console.log(orgArray.filter(num => num % 2 == 0));
+console.log(orgArray.reduce((acc, value) => acc + value, 0));
+console.log(orgArray.reduceRight((acc, value) => acc - value, 0));
+
